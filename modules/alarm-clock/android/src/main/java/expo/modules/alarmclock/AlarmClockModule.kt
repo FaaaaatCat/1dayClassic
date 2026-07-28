720b682e53d41d64c61872e30d8af307dec2c7b3
Taejoeng Kwak <137490818+FaaaaatCat@users.noreply.github.com>
2026-07-28 16:11:51 +0900

    feat(alarm): 로컬 Expo 모듈 alarm-clock 스캐폴딩

---
+++ modules/alarm-clock/android/src/main/java/expo/modules/alarmclock/AlarmClockModule.kt
@@ -0,0 +1,10 @@
+package expo.modules.alarmclock
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class AlarmClockModule : Module() {
+  override fun definition() = ModuleDefinition {
+    Name("AlarmClockModule")
+  }
+}
